e469a1750a5d455bd488951b201a619450a15db3
Ryan Hansberry <rhansby@gmail.com>
2014-02-01 14:13:33 -0800

    Wow such dogecoin begging

--- public/js/very_script.js
+++ public/js/very_script.js
@@ -1,3 +1,9 @@
 window.onload = function() {
     console.log('wow');
+
+    var dogecoin_click = document.getElementById('dogecoin-click');
+    dogecoin_click.onclick = function() {
+        dogecoin_click.style.display = 'none';
+        document.getElementById('dogecoin-address').style.display = 'inline';
+    };
 };
